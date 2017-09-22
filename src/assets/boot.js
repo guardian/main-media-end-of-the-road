e5c99637075cc31cc888621216c5f874bf45477f
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-09-22 10:02:52 -0400

    update boot

--- src/assets/boot.js
+++ src/assets/boot.js
@@ -7,6 +7,13 @@ define([], function() {
             '.immersive-main-media__headline-container, .article__header-info {' + 
             '       display: none;' +
             '}' +
+            '.tonal--tone-feature .tonal__main .drop-cap, .tonal--tone-feature .tonal__main .element-pullquote cite {' +
+            '   color: #fff200' +
+            '}' +
+            '.content--immersive-article .in-body-link--immersive {' +
+                'color: #222;' +
+                'border-bottom-color: #ffb000;' +
+            '}' +
             '</style>';
 
             el.innerHTML = html;
